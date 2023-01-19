851fea59ee2b121d537cc45ce61f681dd1f47a5a
Mateus V. Farias <me@mateusfarias.com.br>
2023-01-19 00:30:13 -0400

    feat: add query to read all `Habits` records from the database

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import Fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 const app = Fastify()
+const prisma = new PrismaClient()
 
-app.get('/hello', () => {
-  return 'Hello World'
+app.get('/habits', async () => {
+  const habits = await prisma.habit.findMany()
+
+  return habits
 })
 
 app.listen({
