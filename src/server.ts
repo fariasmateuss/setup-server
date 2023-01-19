02d19e20e336382b574afd1563f57f910dc784f9
Mateus V. Farias <me@mateusfarias.com.br>
2023-01-18 23:21:25 -0400

    chore(deps): add typescript & tsx

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import Fastify from 'fastify'
+
+const app = Fastify()
+
+app.get('hello', () => {
+  return 'Hello World'
+})
+
+app.listen({
+  port: 3333
+}).then(() => {
+  console.log('Server running at http://localhost:3333')
+})
